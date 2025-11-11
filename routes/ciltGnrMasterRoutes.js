83316f2ea6ece042841d6058560daab1eb264cf4
alqoroni <fathurrhazaq@gmail.com>
2025-11-11 15:15:18 +0700

    fix: update enable/disable routes to use correct controller methods

--- routes/ciltGnrMasterRoutes.js
+++ routes/ciltGnrMasterRoutes.js
@@ -5,7 +5,7 @@ const masterGnrController = require("../controllers/ciltGnrMasterController");
 router.get("/", masterGnrController.getAllMasterGNR);
 router.post("/create", masterGnrController.createGNR);
 router.put("/update/:id", masterGnrController.updateGNR);
-router.patch("/enable/:id", masterGnrController.updateGNR);
-router.patch("/disable/:id", masterGnrController.updateGNR);
+router.patch("/enable/:id", masterGnrController.enabledGNR);
+router.patch("/disable/:id", masterGnrController.disabledGNR);
 
 module.exports = router;
