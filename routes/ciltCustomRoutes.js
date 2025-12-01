7897d2370af449f9b62fcc9cf1777c0a9f406b29
Ersa Oktavian Ramadan <ersaoktavianramadan@email.com>
2025-12-01 11:27:54 +0700

    routes switch

--- routes/ciltCustomRoutes.js
+++ routes/ciltCustomRoutes.js
@@ -12,8 +12,8 @@ router.delete("/delete/:id", ciltCustomController.deleteCustomData);
 router.put("/update-with-relations/:id", ciltCustomController.updatePackageWithRelations);
 
 // Package Designer routes (tb_CILT_custom_packages)
-router.get("/packages", ciltCustomController.getCustomPackages);
 router.get("/packages/:id", ciltCustomController.getCustomPackageById);
+router.get("/packages", ciltCustomController.getCustomPackages);
 router.post("/packages/create", ciltCustomController.createCustomPackage);
 router.put("/packages/update/:id", ciltCustomController.updateCustomPackage);
 router.delete("/packages/delete/:id", ciltCustomController.deleteCustomPackage);
